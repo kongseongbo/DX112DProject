524b196fa207808804fec45b5a50f426a441ce61
[kongseongbo] <[gwg4662@gmail.com]>
2023-03-08 19:59:05 +0900

    ..

---
+++ Engine_SOURCE/yaAnimation.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "yaEntity.h"
+
+//using namespace ya::math;
+//using namespace ya::graphics;
+namespace ya
+{
+	class Animation : public Entity
+	{
+	public:
+		
+
+	private:
+
+	};
+}
